bb3db15ef297a9f8ea9f8fa0f7a9787b3d9bb051
mayurmehar003 <72783460+mayurmehar003@users.noreply.github.com>
2023-07-13 18:49:08 +0530

    Product of n natural number

---
+++ Program2.java
@@ -0,0 +1,20 @@
+package day1;
+
+class Program2
+{
+	public static void main(String[] args)
+	{
+		int num=5;
+		
+		int product=1;
+		
+		for(int i=1;i<=num;i++)
+		{
+			product=product*i;
+		}
+		
+		System.out.println(product);
+		
+	}
+	
+}
